ed51736afca129c6f99f3f6564e2651376c7044d
crowdbotics-apps <team@crowdbotics.com>
2024-05-30 18:10:26 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class TestModel(models.Model):
+    "Generated Model"
+    test_field = models.BigIntegerField()
--- backend/life_optimizer_48032/wsgi.py
+++ backend/life_optimizer_48032/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "life_optimizer_48032.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "life_optimizer_48032.settings")
 
 application = get_wsgi_application()
